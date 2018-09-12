83820ccfb8bfd24b6827122b6b1c0cde4e1ef264
padivalley <it.kalaborang@gmail.com>
2018-09-12 09:20:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/itsupport/kotlinexamplecollabwithcloud/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.itsupport.kotlinexamplecollabwithcloud
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
