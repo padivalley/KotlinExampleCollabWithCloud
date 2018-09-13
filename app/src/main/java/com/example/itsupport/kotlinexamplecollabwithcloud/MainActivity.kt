56c41b5d431fd9e5a7cd3d7ca3c34e5e68d5c741
cloudymous <sayacloud@gmail.com>
2018-09-13 13:32:09 +0800

    Menambah fungsi intent untuk memulai activity karyawan di main menu.

--- app/src/main/java/com/example/itsupport/kotlinexamplecollabwithcloud/MainActivity.kt
+++ app/src/main/java/com/example/itsupport/kotlinexamplecollabwithcloud/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.itsupport.kotlinexamplecollabwithcloud
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val proses = (findViewById(R.id.Proses)) as Button
+        val karyawanMenu = (findViewById(R.id.Karyawan)) as Button
 
         proses.setOnClickListener {
             val valJumlahPesanan = (findViewById(R.id.JumlahPesanan)) as EditText
@@ -23,6 +26,16 @@ class MainActivity : AppCompatActivity() {
             val hasilnya =  hitungPesanan(jumlahPesananEt, hargaPesananEt)
             val tampilHasil = (findViewById(R.id.Pembayaran)) as TextView
             tampilHasil.setText(getString(R.string.hasil) + " " + hasilnya )
+        }
+
+        karyawanMenu.setOnClickListener{
+            val intentKaryawan = Intent(this, KaryawanClass::class.java)
+
+            if (intentKaryawan.resolveActivity(packageManager) != null) {
+                startActivity(intentKaryawan)
+            } else {
+                Log.d("ImplicitIntents", "Can't handle this intent!")
+            }
         }
     }
 
