e69c1186775363379e4eb20df9834058c68714f7
cloudymous <sayacloud@gmail.com>
2018-10-18 16:52:07 +0800

    Buat Activity Utama baru.

---
+++ app/src/main/java/com/example/itsupport/kotlinexamplecollabwithcloud/Utama.kt
@@ -0,0 +1,12 @@
+package com.example.itsupport.kotlinexamplecollabwithcloud
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Utama : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_utama)
+    }
+}
