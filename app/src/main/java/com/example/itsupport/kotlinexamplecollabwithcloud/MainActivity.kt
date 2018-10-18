6dd348b1724a5b5dfdfe7a93fe36371698341327
cloudymous <sayacloud@gmail.com>
2018-10-18 16:13:59 +0800

    Ubah kode pakai Kotlin Android Plugins. Lebih simple. Bye findViewById.

--- app/src/main/java/com/example/itsupport/kotlinexamplecollabwithcloud/MainActivity.kt
+++ app/src/main/java/com/example/itsupport/kotlinexamplecollabwithcloud/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,21 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val proses = (findViewById(R.id.Proses)) as Button
-        val karyawanMenu = (findViewById(R.id.Karyawan)) as Button
-
-        proses.setOnClickListener {
-            val valJumlahPesanan = (findViewById(R.id.JumlahPesanan)) as EditText
-            val jumlahPesananEt = Integer.parseInt(valJumlahPesanan.text.toString())
-            val valHargaPesanan = (findViewById(R.id.HargaPesanan)) as EditText
-            val hargaPesananEt = Integer.parseInt(valHargaPesanan.text.toString())
+        Proses.setOnClickListener {
+            val jumlahPesananEt = Integer.parseInt(JumlahPesanan.text.toString())
+            val hargaPesananEt = Integer.parseInt(HargaPesanan.text.toString())
 
             val hasilnya =  hitungPesanan(jumlahPesananEt, hargaPesananEt)
-            val tampilHasil = (findViewById(R.id.Pembayaran)) as TextView
-            tampilHasil.setText(hasilnya)
+            Pembayaran.setText(hasilnya)
         }
 
-        karyawanMenu.setOnClickListener{
+        Karyawan.setOnClickListener{
             val intentKaryawan = Intent(this, TampilanMenu::class.java)
 
             if (intentKaryawan.resolveActivity(packageManager) != null) {
